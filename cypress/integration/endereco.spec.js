487ba4f900ade68203fa6c2f56b1460c3e035611
brunodsn <dsnunesbruno@gmail.com>
2023-03-16 16:41:54 +0000

    Aula-Cypress Modulo 12

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require ('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -11,6 +13,25 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     });
 
     it('Deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento ('Bruno', 'Nunes', 'Google', 'Brasil', 'Rua Pedro Polozzi', '23', 'Louveira', 'São Paulo', '13290000', '1234567890', 'brunonunes@ebac.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+
+    });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivos de dados - - Usando arquivos de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento (
+            dadosEndereco [1].nome,
+            dadosEndereco [1].sobrenome,
+            dadosEndereco [1].empresa,
+            dadosEndereco [1].pais,
+            dadosEndereco [1].numero,
+            dadosEndereco [1].cidade,
+            dadosEndereco [1].estado,
+            dadosEndereco [1].cep,
+            dadosEndereco [1].telefone,
+            dadosEndereco [1].email
+            )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
 
         //cadastro de endereço
     });
